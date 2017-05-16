e96c6c7a3ee2b88c7b46e64b30dae32e1c7418c8
JonathanMarquina <jmarquinar@upao.edu.pe>
2017-05-16 01:29:03 -0500

    Update Almnos.php

--- Almnos.php
+++ Almnos.php
@@ -1,5 +1,21 @@
 <?php
 Class alumno
 {
-
+  Public $Primer_Nombre;
+  Public $Segundo_Nombre;
+  Piblic $Nota;
+  Public function __construct($Primer_Nombre, $Segundo_Nombre, $Nota)
+  { $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+    public function fullName()
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
 }
+$person1 = new Person('Duilio', 'Palacios');
+$person2 = new Person('Ramon', 'Lapenta');
+//
+echo "{$person1->fullName()} es amigo de {$person2->fullName()}";
+
+    
