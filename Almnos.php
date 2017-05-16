38e29cb7b1fccb06a8a7850e65ceb7bfa4b16e82
JonathanMarquina <jmarquinar@upao.edu.pe>
2017-05-16 12:41:17 -0500

    Update Almnos.php

--- Almnos.php
+++ Almnos.php
@@ -1,21 +1,29 @@
 <?php
-Class alumno
+Class Alumno
 {
   Public $Primer_Nombre;
   Public $Segundo_Nombre;
   Piblic $Nota;
   Public function __construct($Primer_Nombre, $Segundo_Nombre, $Nota)
-  { $this->firstName = $firstName;
-        $this->lastName = $lastName;
+  { 
+     $this->Primero_Nombre = $Primero_Nombre;
+     $this->Segundo_Nombre = $Segundo_Nombre;
+     $this->Nota = $Nota;
     }
     public function fullName()
     {
-        return $this->firstName . ' ' . $this->lastName;
+        return $this->Primer_Nombre . ' ' . $this->Segundo_Nombre;
+    }
+    public function Notas()
+    {
+      return $this->Nota;
     }
 }
-$person1 = new Person('Duilio', 'Palacios');
-$person2 = new Person('Ramon', 'Lapenta');
-//
-echo "{$person1->fullName()} es amigo de {$person2->fullName()}";
+$alumno1 = new Alumno('Julio', 'Pereda');
+$alumno2 = new Alumno('Jhon', 'Reyes');
+$nota1 = new Alumno(12);
+$nota2 = new Alumno(18);
 
-    
+//
+echo "{$alumno1->fullName()} tiene una nota de {$nota1->Notas()}";
+echo "{$alumno2->fullName()} tiene una nota de {$nota2->Notas()}";
